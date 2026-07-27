266ca9146b6a28b7f6128eec68740dd0d8105a30
Atmik Aher <atmikaher439@gmail.com>
2026-07-27 13:27:38 +0530

    Variables

--- Day 1/basic.py
+++ Day 1/basic.py
@@ -1,1 +1,13 @@
 print("Hello, Atmik!")
+# This is a simple Python program that prints what is inside the quotation marks.
+
+# - is denoted as a comment in Python. It is used to explain what the code does.
+
+## Variables are used to store data in Python. In this case, we are storing the string "Atmik"
+first_name = "Atmik"
+print(first_name) #this line will print the value of the variable first_name, which is "Atmik".
+
+Atmik = 15
+age = 22.1
+
+# variable can be stored in many other formats like Number, boolane, float, etc
