ed6430291dde696ac07cb0269682f77197c35f8d
Atmik Aher <atmikaher439@gmail.com>
2026-08-06 12:05:47 +0530

    Strings

--- Day 1/basic.py
+++ Day 1/basic.py
@@ -24,3 +24,23 @@ print(type(42))       # <class 'int'>
 print(type(3.14))     # <class 'float'>
 print(type("hello"))  # <class 'str'>
 print(type(True))     # <class 'bool'>
+
+#String -
+a = "Atmik"
+print(a[0], a[-5])
+#Slice Function-
+print(a[0:3:1])
+b = "COLLEGE"
+print(b[0:8:2])
+#Defalt Values -
+print(b[::2])
+
+c = "Hellow how are you"
+#print how
+print(c[7:10:1])
+#print you
+print(c[15::1])
+#print Hellow
+print(c[:6:1])
+
+print(c[::-1])
