b45f8555478ed71a4a84ecba3d531dfa4c6045c6
Atmik Aher <atmikaher439@gmail.com>
2026-09-06 19:15:48 +0530

    File Handling

---
+++ Day 2/File Handling.py
@@ -0,0 +1,18 @@
+#File Handling
+
+#To create New file
+# open("hellow.txt", "x")
+
+#To create and write something in file
+#file = open("batman.txt", "w")
+
+#To add text into file using python
+#data = input("what's password - ")
+
+#save the data in txt file
+#file.write(data)
+
+# Reading file
+
+#file = open("Exception Handling.py", "r")
+#print(file.read())
